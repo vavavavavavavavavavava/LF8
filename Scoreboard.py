cff88c3fa576e26bec961f7770afd63437aa7195
Jonah <jonah.ludemann@gmail.com>
2025-04-07 14:12:11 +0200

    Scoreboard docstrings added

--- Scoreboard.py
+++ Scoreboard.py
@@ -1,13 +1,41 @@
+"""
+Scoreboard Module
+
+This module provides a `Scoreboard` class to interact with a database of highscores.
+It allows retrieving and submitting highscores using the `getData` module.
+"""
+
 import getData
 
 class Scoreboard:
+    """
+    A class to manage highscores.
+
+    This class provides methods to retrieve and submit highscores
+    to a database using the `getData` module.
+    """
+
     def __init__(self):
+        """
+        Initializes the Scoreboard instance.
+        """
         pass
 
     def get_highscores(self):
-        """Holt die Highscores aus der Datenbank."""
+        """
+        Retrieves the highscores from the database.
+
+        Returns:
+            list: A list of highscores retrieved from the database.
+        """
         return getData.get_highscore()
 
     def submit_highscore(self, name, score):
-        """Speichert einen neuen Highscore in der Datenbank."""
+        """
+        Submits a new highscore to the database.
+
+        Args:
+            name (str): The name of the player.
+            score (int): The score achieved by the player.
+        """
         getData.set_highscore(name, score)
