8611c1e6f2fcdf1c7d9c4ec2a5ec19a1415c5b22
Jonah <jonah.ludemann@gmail.com>
2025-04-07 14:07:56 +0200

    question.py docstrings added

--- Question.py
+++ Question.py
@@ -1,16 +1,43 @@
+"""
+This module defines the `Question` class, which represents a Pokémon quiz question.
+It retrieves Pokémon data such as names and images and generates multiple-choice answers.
+"""
+
 import random
 import getData
 
 class Question:
-    def __init__(self, pokedexNr):
-        self.pokedexNr = pokedexNr
-        self.correct_answer = getData.get_pokemon_name(pokedexNr)
-        self.original_image = getData.get_pokemon_image(pokedexNr)
-        self.black_image = getData.get_black_image(pokedexNr)
+    """
+    Represents a Pokémon quiz question.
+
+    Attributes:
+        pokedex_number (int): The Pokédex number of the Pokémon.
+        correct_answer (str): The correct name of the Pokémon.
+        original_image (str): The URL or path to the original Pokémon image.
+        black_image (str): The URL or path to the blacked-out Pokémon image.
+        choices (list): A list of answer choices, including the correct answer.
+    """
+
+    def __init__(self, pokedex_number):
+        """
+        Initializes a Question instance.
+
+        Args:
+            pokedex_number (int): The Pokédex number of the Pokémon.
+        """
+        self.pokedex_number = pokedex_number
+        self.correct_answer = getData.get_pokemon_name(pokedex_number)
+        self.original_image = getData.get_pokemon_image(pokedex_number)
+        self.black_image = getData.get_black_image(pokedex_number)
         self.choices = self.generate_choices()
 
     def generate_choices(self):
-        """Erstellt eine Liste mit Antwortmöglichkeiten, einschließlich der richtigen Antwort."""
+        """
+        Generates a list of answer choices, including the correct answer.
+
+        Returns:
+            list: A shuffled list of four Pokémon names, one of which is the correct answer.
+        """
         choices = [self.correct_answer]
         while len(choices) < 4:
             choice = getData.get_pokemon_name(random.randint(1, 1025))
@@ -20,13 +47,37 @@ class Question:
         return choices
 
     def get_correct_answer(self):
+        """
+        Retrieves the correct answer for the question.
+
+        Returns:
+            str: The correct Pokémon name.
+        """
         return self.correct_answer
 
     def get_choices(self):
+        """
+        Retrieves the list of answer choices.
+
+        Returns:
+            list: A list of Pokémon names.
+        """
         return self.choices
 
     def get_black_image(self):
+        """
+        Retrieves the blacked-out image of the Pokémon.
+
+        Returns:
+            str: The URL or path to the blacked-out Pokémon image.
+        """
         return self.black_image
 
     def get_original_image(self):
+        """
+        Retrieves the original image of the Pokémon.
+
+        Returns:
+            str: The URL or path to the original Pokémon image.
+        """
         return self.original_image
