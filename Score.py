5261bb889a7be49f33438ab057f22bafbfc488c6
Jonah <jonah.ludemann@gmail.com>
2025-04-07 14:10:50 +0200

    Score Docstrings added

--- Score.py
+++ Score.py
@@ -1,9 +1,29 @@
+"""
+This module defines the Score class, which is used to manage and track a score value.
+"""
+
 class Score:
+    """
+    A class to represent and manage a score.
+    """
+
     def __init__(self):
+        """
+        Initialize the Score object with a score value of 0.
+        """
         self.score = 0
 
     def increase(self):
+        """
+        Increment the score by 1.
+        """
         self.score += 1
 
     def get(self):
+        """
+        Retrieve the current score value.
+
+        Returns:
+            int: The current score.
+        """
         return self.score
